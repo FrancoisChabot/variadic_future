bcba1633cec02e406e767656989e4dcfbced000f
Francois Chabot <francois@ageofminds.com>
2019-06-08 14:31:42 -0400

    better future<int> test coverage

--- tests/int.cpp
+++ tests/int.cpp
@@ -21,6 +21,56 @@
 
 using namespace aom;
 
+namespace {
+
+  struct prom_fut {
+    prom_fut() {
+      f = p.get_future();
+    }
+
+    int get() {
+      return f.get_std_future().get();
+    }
+
+    Promise<int> p;
+    Future<int> f;
+  };
+
+  struct pf_set {
+    prom_fut pf[4];
+
+    prom_fut& operator[](std::size_t i) {return pf[i];}
+
+    void complete() {
+      pf[0].p.set_value(1);
+      pf[1].p.set_exception(std::make_exception_ptr(std::logic_error("nope")));
+      pf[2].p.finish(expected<int>(1));
+      pf[3].p.finish(unexpected(std::make_exception_ptr(std::logic_error(""))));
+    }
+  };
+
+  void no_op(int i) {
+    EXPECT_EQ(i, 1);
+  }
+
+  void failure(int i) {
+    EXPECT_EQ(i, 1);
+    throw std::runtime_error("dead");
+  }
+
+  int expect_noop_count = 0;
+  int expected_noop(expected<int>) {
+    ++expect_noop_count;
+    return 1;
+  }
+
+  void expected_noop_fail(expected<int>) {
+    throw std::runtime_error("dead");
+  }
+
+
+}
+
 TEST(Future_int, blank) {
   Future<int> fut;
 }
@@ -35,176 +85,199 @@ TEST(Future_int, unfilled_promise_failiure) {
   EXPECT_THROW(fut.get_std_future().get(), Unfullfilled_promise);
 }
 
+TEST(Future_int, preloaded_std_get) {
+  pf_set pf;
 
-TEST(Future_int, then_success) {
-  {
-    Promise<int> p;
-    auto f = p.get_future();
+  pf.complete();
 
-    auto two = f.then([](int i){ return i+1;});
-    p.set_value(1);
-
-    EXPECT_EQ(2, two.get_std_future().get());
-  }
-  {
-    Promise<int> p;
-    auto f = p.get_future();
-
-    p.set_value(1);
-    auto two = f.then([](int i){ return i+1;});
-
-    EXPECT_EQ(2, two.get_std_future().get());
-  }
+  EXPECT_EQ(1,      pf[0].get());
+  EXPECT_THROW(     pf[1].get(), std::logic_error);
+  EXPECT_EQ(1,      pf[2].get());
+  EXPECT_THROW(     pf[3].get(), std::logic_error);
 }
 
-TEST(Future_int, then_failure) {
-  {
-    Promise<int> p;
-    auto f = p.get_future();
+TEST(Future_int, delayed_std_get) {
+  pf_set pf;
 
-    auto one = f.then([](int){ throw std::runtime_error("nope");});
-    p.set_value(1);
+  std::mutex mtx;
+  std::unique_lock l(mtx);
+  std::thread thread([&] {
+    std::lock_guard ll(mtx);
+    pf.complete();
+  });
 
-    EXPECT_THROW(one.get_std_future().get(), std::runtime_error);
-  }
-  {
-    Promise<int> p;
-    auto f = p.get_future();
+  auto std_f1 = pf[0].f.get_std_future();
+  auto std_f2 = pf[1].f.get_std_future();
+  auto std_f3 = pf[2].f.get_std_future();
+  auto std_f4 = pf[3].f.get_std_future();
+  
+  l.unlock();
+  
+  EXPECT_EQ(1, std_f1.get());
+  EXPECT_THROW(std_f2.get(), std::logic_error);
+  EXPECT_EQ(1, std_f3.get());
+  EXPECT_THROW(std_f4.get(), std::logic_error);
 
-    p.set_value(1);
-    auto one = f.then([](int){ throw std::runtime_error("nope");});
-
-    EXPECT_THROW(one.get_std_future().get(), std::runtime_error);
-  }
-}
-
-TEST(Future_int, then_failure_transit) {
-  {
-    Promise<int> p;
-    auto f = p.get_future();
-
-    auto one = f.then([](int i){ return i+1;});
-    p.set_exception(std::make_exception_ptr(std::runtime_error("nope")));
-
-    EXPECT_THROW(one.get_std_future().get(), std::runtime_error);
-  }
-  {
-    Promise<int> p;
-    auto f = p.get_future();
-
-    p.set_exception(std::make_exception_ptr(std::runtime_error("nope")));
-    auto one = f.then([](int i){ return i+1;});
-
-    EXPECT_THROW(one.get_std_future().get(), std::runtime_error);
-  }
+  thread.join();
 }
 
 
-TEST(Future_int, then_expect_success) {
-  {
-    Promise<int> p;
-    auto f = p.get_future();
+TEST(Future_int, then_noop_pre) {
+  pf_set pf;
 
-    auto one = f.then_expect([](expected<int> v){ return v.value() + 1;});
-    p.set_value(1);
+  auto f1 = pf[0].f.then(no_op);
+  auto f2 = pf[1].f.then(no_op);
+  auto f3 = pf[2].f.then(no_op);
+  auto f4 = pf[3].f.then(no_op);
 
-    EXPECT_EQ(2, one.get_std_future().get());
-  }
-  {
-    Promise<int> p;
-    auto f = p.get_future();
-
-    p.set_value(1);
-    auto one = f.then_expect([](expected<int> v){ return v.value() + 1;});
-
-    EXPECT_EQ(2, one.get_std_future().get());
-  }
+  pf.complete();
+  
+  EXPECT_NO_THROW(f1.get_std_future().get());
+  EXPECT_THROW(f2.get_std_future().get(), std::logic_error);
+  EXPECT_NO_THROW(f3.get_std_future().get());
+  EXPECT_THROW(f4.get_std_future().get(), std::logic_error);
 }
 
-TEST(Future_int, then_expect_failure) {
-  {
-    Promise<int> p;
-    auto f = p.get_future();
+TEST(Future_int, then_noop_post) {
+  pf_set pf;
 
-    auto one = f.then_expect([](expected<int> v){ EXPECT_TRUE(v.has_value()); throw std::runtime_error("nope");});
-    p.set_value(1);
+  pf.complete();
 
-    EXPECT_THROW(one.get_std_future().get(), std::runtime_error);
-  }
-  {
-    Promise<int> p;
-    auto f = p.get_future();
-
-    p.set_value(1);
-    auto one = f.then_expect([](expected<int> v){ EXPECT_TRUE(v.has_value()); throw std::runtime_error("nope");});
-
-    EXPECT_THROW(one.get_std_future().get(), std::runtime_error);
-  }
+  auto f1 = pf[0].f.then(no_op);
+  auto f2 = pf[1].f.then(no_op);
+  auto f3 = pf[2].f.then(no_op);
+  auto f4 = pf[3].f.then(no_op);
+  
+  EXPECT_NO_THROW(f1.get_std_future().get());
+  EXPECT_THROW(f2.get_std_future().get(), std::logic_error);
+  EXPECT_NO_THROW(f3.get_std_future().get());
+  EXPECT_THROW(f4.get_std_future().get(), std::logic_error);
 }
 
-TEST(Future_int, then_expect_intercept) {
-  {
-    Promise<int> p;
-    auto f = p.get_future();
+TEST(Future_int, then_failure_pre) {
+  pf_set pf;
 
-    auto one = f.then_expect([](expected<int> v){ EXPECT_FALSE(v.has_value()); return 1;});
-    p.set_exception(std::make_exception_ptr(std::runtime_error("nope")));
+  auto f1 = pf[0].f.then(failure);
+  auto f2 = pf[1].f.then(failure);
+  auto f3 = pf[2].f.then(failure);
+  auto f4 = pf[3].f.then(failure);
+  
+  pf.complete();
 
-    EXPECT_EQ(1, one.get_std_future().get());
-  }
-  {
-    Promise<int> p;
-    auto f = p.get_future();
-
-    p.set_exception(std::make_exception_ptr(std::runtime_error("nope")));
-    auto one = f.then_expect([](expected<int> v){ EXPECT_FALSE(v.has_value()); return 1;});
-
-    EXPECT_EQ(1, one.get_std_future().get());
-  }
+  EXPECT_THROW(f1.get_std_future().get(), std::runtime_error);
+  EXPECT_THROW(f2.get_std_future().get(), std::logic_error);
+  EXPECT_THROW(f3.get_std_future().get(), std::runtime_error);
+  EXPECT_THROW(f4.get_std_future().get(), std::logic_error);
 }
 
-TEST(Future_int, then_expect_finally_success) {
-  {
-    Promise<int> p;
-    auto f = p.get_future();
+TEST(Future_int, then_failure_post) {
+  pf_set pf;
 
-    int dst = 0;
-    f.then_finally_expect([&](expected<int> v){ EXPECT_TRUE(v.has_value()); dst = *v + 1;});
-    p.set_value(1);
+  pf.complete();
 
-    EXPECT_EQ(2, dst);
-  }
-  {
-    Promise<int> p;
-    auto f = p.get_future();
+  auto f1 = pf[0].f.then(failure);
+  auto f2 = pf[1].f.then(failure);
+  auto f3 = pf[2].f.then(failure);
+  auto f4 = pf[3].f.then(failure);
 
-    p.set_value(1);
-    int dst = 0;
-    f.then_finally_expect([&](expected<int> v){EXPECT_TRUE(v.has_value()); dst = *v + 1;});
-
-    EXPECT_EQ(2, dst);
-  }
+  EXPECT_THROW(f1.get_std_future().get(), std::runtime_error);
+  EXPECT_THROW(f2.get_std_future().get(), std::logic_error);
+  EXPECT_THROW(f3.get_std_future().get(), std::runtime_error);
+  EXPECT_THROW(f4.get_std_future().get(), std::logic_error);
 }
 
-TEST(Future_int, then_expect_finally_intercept) {
-  {
-    Promise<int> p;
-    auto f = p.get_future();
-    
-    int dst = 0;
-    f.then_finally_expect([&](expected<int> v){ EXPECT_FALSE(v.has_value()); dst= 1;});
-    p.set_exception(std::make_exception_ptr(std::runtime_error("nope")));
+TEST(Future_int, then_expect_success_pre) {
+  pf_set pf;
 
-    EXPECT_EQ(1, dst);
-  }
-  {
-    Promise<int> p;
-    auto f = p.get_future();
+  auto f1 = pf[0].f.then_expect(expected_noop);
+  auto f2 = pf[1].f.then_expect(expected_noop);
+  auto f3 = pf[2].f.then_expect(expected_noop);
+  auto f4 = pf[3].f.then_expect(expected_noop);
+  
+  pf.complete();
 
-    p.set_exception(std::make_exception_ptr(std::runtime_error("nope")));
-    int dst = 0;
-    f.then_finally_expect([&](expected<int> v){ EXPECT_FALSE(v.has_value()); dst = 1;});
+  EXPECT_EQ(1, f1.get_std_future().get());
+  EXPECT_EQ(1, f2.get_std_future().get());
+  EXPECT_EQ(1, f3.get_std_future().get());
+  EXPECT_EQ(1, f4.get_std_future().get());
 
-    EXPECT_EQ(1, dst);
-  }
+}
+
+TEST(Future_int, then_expect_success_post) {
+  pf_set pf;
+
+  auto f1 = pf[0].f.then_expect(expected_noop);
+  auto f2 = pf[1].f.then_expect(expected_noop);
+  auto f3 = pf[2].f.then_expect(expected_noop);
+  auto f4 = pf[3].f.then_expect(expected_noop);
+  
+  pf.complete();
+
+  EXPECT_EQ(1, f1.get_std_future().get());
+  EXPECT_EQ(1, f2.get_std_future().get());
+  EXPECT_EQ(1, f3.get_std_future().get());
+  EXPECT_EQ(1, f4.get_std_future().get());
+}
+
+TEST(Future_int, then_expect_failure_pre) {
+  pf_set pf;
+
+  pf.complete();
+
+  auto f1 = pf[0].f.then_expect(expected_noop_fail);
+  auto f2 = pf[1].f.then_expect(expected_noop_fail);
+  auto f3 = pf[2].f.then_expect(expected_noop_fail);
+  auto f4 = pf[3].f.then_expect(expected_noop_fail);
+
+  EXPECT_THROW(f1.get_std_future().get(), std::runtime_error);
+  EXPECT_THROW(f2.get_std_future().get(), std::runtime_error);
+  EXPECT_THROW(f3.get_std_future().get(), std::runtime_error);
+  EXPECT_THROW(f4.get_std_future().get(), std::runtime_error);
+}
+
+TEST(Future_int, then_expect_failure_post) {
+  pf_set pf;
+
+  auto f1 = pf[0].f.then_expect(expected_noop_fail);
+  auto f2 = pf[1].f.then_expect(expected_noop_fail);
+  auto f3 = pf[2].f.then_expect(expected_noop_fail);
+  auto f4 = pf[3].f.then_expect(expected_noop_fail);
+
+  pf.complete();
+
+  EXPECT_THROW(f1.get_std_future().get(), std::runtime_error);
+  EXPECT_THROW(f2.get_std_future().get(), std::runtime_error);
+  EXPECT_THROW(f3.get_std_future().get(), std::runtime_error);
+  EXPECT_THROW(f4.get_std_future().get(), std::runtime_error);
+}
+
+
+TEST(Future_int, then_expect_finally_success_pre) {
+  pf_set pf;
+
+  auto ref = expect_noop_count;
+  
+  pf[0].f.then_finally_expect(expected_noop);
+  pf[1].f.then_finally_expect(expected_noop);
+  pf[2].f.then_finally_expect(expected_noop);
+  pf[3].f.then_finally_expect(expected_noop);
+
+  pf.complete();
+
+  EXPECT_EQ(4+ref, expect_noop_count);
+}
+
+TEST(Future_int, then_expect_finally_success_post) {
+  pf_set pf;
+
+  auto ref = expect_noop_count;
+
+  pf.complete();
+
+  pf[0].f.then_finally_expect(expected_noop);
+  pf[1].f.then_finally_expect(expected_noop);
+  pf[2].f.then_finally_expect(expected_noop);
+  pf[3].f.then_finally_expect(expected_noop);
+
+  EXPECT_EQ(4+ref, expect_noop_count);
 }
