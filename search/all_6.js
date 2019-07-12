bb99b95830e06f0ee2a2e30ee143e45546d82ae5
VarFutCI <francois.chabot.dev+var_fut@gmail.com>
2019-07-12 03:35:29 +0000

    Automated deployment to GitHub Pages: 0500635f1d5d50ce68ede3949fb5dd2c8c9d80b6

--- search/all_6.js
+++ search/all_6.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['operator_20bool',['operator bool',['../classaom_1_1Basic__promise.html#a8db4e898fc1446a915fd89aea19e8a1c',1,'aom::Basic_promise']]],
+  ['operator_20bool',['operator bool',['../classaom_1_1Basic__promise.html#a8db4e898fc1446a915fd89aea19e8a1c',1,'aom::Basic_promise::operator bool()'],['../classaom_1_1Basic__stream__promise.html#aa5edd55db3aaa50f2839885f6a4cf93b',1,'aom::Basic_stream_promise::operator bool()']]],
   ['operator_3d',['operator=',['../classaom_1_1Basic__future.html#a38b6d93205e59006f1cc8f841aa8571b',1,'aom::Basic_future::operator=()'],['../classaom_1_1Basic__stream__future.html#af8c19e6d0789b3a009bfbaa68c8dd5d4',1,'aom::Basic_stream_future::operator=()'],['../classaom_1_1Basic__stream__promise.html#a3ceaeafafba3c5ab06020023a9f663a1',1,'aom::Basic_stream_promise::operator=()']]]
 ];
